2fa49a791ff8d1c5ed068a245f1d6afc8e7ffd42
Mohammadreza B <b.mohammadreza@gmail.com>
2023-09-15 20:54:27 -0400

    Resume implementation of 'linter.py' to integrate code analyzers. Add coryright statements.

--- linter/linter.py
+++ linter/linter.py
@@ -1,13 +1,70 @@
+# Author: Mohammadreza Bahmanpour
+# Copyright 2023 streamorph.live project
+# All rights reserved. Please refer to LICENSE file for details
+
 # Removed the shebang for now
 
 import sys
 import shutil
+import json
+import os
+
 from colorama import Fore
 
 def print_usage():
     print(Fore.CYAN+'Usage: linter.py --name <linter_cmd_name> --src <path_to_src_dir> --build <path_to_build_dir>')
     sys.exit(1)
 
+def build_settings_path(src_path) -> str:
+    path = os.path.join('linter', '.settings.json')
+    return os.path.join(src_path, path)
+
+def load_settings(settings_path) -> (str, list, list):
+    # TODO: Make sure the settings path is compatible for all 
+    # platforms (Windows)
+
+    with open(settings_path, 'r') as setting_file: 
+        (name, place_holders, options) = json.load(setting_file, object_hook=get_linter_params)
+    
+    return (name, place_holders, options)
+
+def get_linter_params(struct:dict) -> (str, list, list):
+    """ Returns the only active linter parameters """
+
+    name: str = ''
+    place_holders: list = []
+    options: list = []
+
+    print(Fore.GREEN+f'DEBUG >>> get_linter_params(): struct->{struct}')
+    print(Fore.RESET, end='')
+
+    for key in struct.keys():
+        value:dict = struct[key]
+        name = key
+
+        is_enabled = False
+        try:
+            is_enabled = value['enabled']
+        except ValueError as e:
+            print(Fore.RED+f'ERROR: "enabled" setting missed! \
+                  In ".settings.json" each linter must define "enabled" setting. Description: {e}')
+        
+        if is_enabled == False:
+            continue
+
+        try:
+            place_holders = value['place_holders']
+        except ValueError as e:
+            print(Fore.RED+f'ERROR: "place_holders" setting missed! \
+                  In ".settings.json" each linter must define "place_holders" setting. Description: {e}')
+            
+        try:
+            options = value['options']
+        except ValueError as e:
+            print(Fore.RED+f'ERROR: "options" setting missed! \
+                  In ".settings.json" each linter must define "options" setting. Description: {e}')
+    
+    return (name, place_holders, options)
 
 def main():
     if len(sys.argv) < 5:
@@ -45,7 +102,10 @@ def main():
         print(Fore.RED+f'ERROR: Invalid build_path: {build_path}')
         sys.exit(1)
 
-    print(Fore.GREEN+f'DEBUG >>> linter_name->{linter_name}, src_path->{src_path}, build_path->{build_path}')
+    settings_path = build_settings_path(src_path)
+    (name, place_holders, options) = load_settings(settings_path)
+
+    print(Fore.GREEN+f'DEBUG >>> variables: {vars()}')
     print(Fore.RESET, end='')
 
 if __name__ == '__main__':
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+// Author: Mohammadreza Bahmanpour
+// Copyright 2023 streamorph.live project.
+// All rights reserved. Please refer to LICENSE file for details
+
 #include <iostream>
 
 int main(int argc, char* argv[]) {
