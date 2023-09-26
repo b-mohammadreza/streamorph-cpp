2ed0d349d4de96bdcb4f54874e95cff0133f9143
Mohammadreza B <b.mohammadreza@gmail.com>
2023-09-26 16:47:57 -0400

    Integrate gtest and add the first sample test.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 // Copyright 2023 streamorph.live project.
 // All rights reserved. Please refer to LICENSE file for more details.
 
-// #include <iostream>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
     // int i;
-    // std::cout << "Hello from streamorph...." << std::endl;
+    std::cout << "Hello from streamorph...." << std::endl;
     return 0;
 }
---
+++ test/test_main.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(SampleSuite, SampleTest) {
+    EXPECT_STRNE("Hello", "World");
+    EXPECT_EQ(25, 5*4);
+}
