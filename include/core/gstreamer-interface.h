cab3624564287a283ba3613e7d30255518cf9efe
Mohammadreza B <b.mohammadreza@gmail.com>
2023-11-07 14:34:57 -0500

    Start to implement a facade to interface the gstreamer API.

---
+++ include/core/gstreamer-interface.h
@@ -0,0 +1,7 @@
+// Author: Mohammadreza Bahmanpour
+// Copyright 2023 streamorph.live project.
+// All rights reserved. Please refer to LICENSE file for more details.
+
+/* A facade to interface the gstreamer API */
+
+
